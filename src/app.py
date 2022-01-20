663cafd86af305cb377432e8452a5be6a9789f61
unknown <pswestmore72@gmail.com>
2022-01-19 19:39:34 -0600

    pushing basic flask app and loadable home page

--- src/app.py
+++ src/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
    return render_template("home.html")
 
+@app.route("/machine-learning")
+def ml():
+   return render_template("machine_learning.html") 
+
 if __name__ == "__main__":
    app.run()
