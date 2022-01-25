89012abbd94d01970f7f397d689b87f477e03a5d
unknown <pswestmore72@gmail.com>
2022-01-24 20:28:23 -0600

    pushing for something

--- src/app.py
+++ src/app.py
@@ -1,11 +1,44 @@
 from flask import Flask, render_template, redirect, url_for
-from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
    return render_template("home.html")
+
+@app.route("/about")
+def about():
+   data = {
+      'paul': {
+         'description': "",
+         'gh': "",
+         'linked': "",
+      },
+      'chris': {
+         'description': "",
+         'gh': "",
+         'linked': "",
+      },
+      'stuart': {
+         'description': "",
+         'gh': "",
+         'linked': "",
+      },
+      'moez': {
+         'description': "",
+         'gh': "",
+         'linked': "",
+      },
+   }
+   return render_template("about.html", authors=data)
+
+@app.route("/food")
+def food():
+   return render_template("food.html") 
+
+@app.route("/weather")
+def weather():
+   return render_template("weather.html")   
 
 @app.route("/machine-learning")
 def ml():
